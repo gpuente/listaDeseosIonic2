d2057112322224a65d4891b376f853356c291d7d
Guillermo Puente <guillermo.ps09@gmail.com>
2017-05-08 00:26:07 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { PendientesComponent } from "../pages/pendientes/pendientes.component";
+import { TerminadosComponent } from "../pages/terminados/terminados.component";
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
